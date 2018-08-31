f3ef381c3e67bbab87404cfe192ca752fc041913
Jeffresh <jeff.uca@gmail.com>
2018-08-31 22:57:36 +0200

    Agregadas definiciones de funciones anadir, del y value sin terminar

--- function.hpp
+++ function.hpp
@@ -6,16 +6,16 @@ class Function
 
 public:
 
-	struct
+	struct Punto
 	{
 		double x_,y_;
 		Punto(double x = 0, double y = 0):x_(x),y_(y){}
 	};
 
 
-	Function(double x, double y);
-	void Add(double x, double y);
-	void del(double x);
+	Function(double , double );
+	void Add(double, double );
+	void del(double);
 	double value(double x)const;
 	double min()const;
 	double max()const;
@@ -32,4 +32,55 @@ private:
 
 };
 
-#endif
+
+
+inline Function::Function(double x, double y) { saltos.insertar(Punto(x,y),saltos.primera());}
+
+void Function::dad(double x, double y)
+{
+	Lista<Punto>::posicion p = saltos.primera();
+
+	while(p != saltos.fin() && saltos.elemento(p).x_ < x)
+		p = p.siguiente(p);
+
+	if(p!= saltos.fin() && saltos.elemento(p).x_==x)
+		saltos.elemento(p).y_ = y;
+	else if(p!= saltos.fin())
+			saltos.insertar(Punto(x,y), p);
+
+
+
+}
+
+void Function::del(double x)
+{
+	Lista<Punto>::posicion p = saltos.primera();
+
+	while(p!= saltos.fin() && saltos.elemento(p).x_ < x)
+		p = p.siguiente(p);
+
+
+	if(p!=saltos.fin() && p!=saltos.primera() && saltos.elementos)
+		saltos.eliminar(saltos.anterior(p));
+
+	if(p!=saltos.fin() && p!=saltos.primera() && saltos.elemento(saltos.anterior(p)).y_ == saltos.elemento(p).y_)
+		saltos.eliminar(p);
+
+
+
+}
+
+double Function::value(double x)
+{
+	Lista<Punto>::posicion p = saltos.primera();
+	while(p!=saltos.fin() && saltos.elemento().x_< x)
+		p = saltos.siguiente(p);
+
+
+
+
+}
+
+
+
+#endif //FUNCTION_HPP_
